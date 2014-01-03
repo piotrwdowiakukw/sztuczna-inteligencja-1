e48953618eb15bdffba8ca33752a9018c5f251d3
Piotr Wdowiak <piotrwdowiak@op.pl>
2014-01-03 17:07:48 +0100

    just another repository version, go on

--- src/Rysunek.java
+++ src/Rysunek.java
@@ -7,11 +7,14 @@ import javax.swing.JPanel;
 public class Rysunek extends JPanel {
 	
 	public Rysunek() {
-		setBackground(Color.BLUE);		
+		setBackground(Color.ORANGE);		
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		
+		g.setColor(Color.BLACK);
+		g.drawLine(0, 0, 400, 300);
 	}
 
 }
