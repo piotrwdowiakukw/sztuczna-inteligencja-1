52fdc493198e01c8d246108682dae02c0ba4e172
Piotr Wdowiak <piotrwdowiak@op.pl>
2014-01-03 17:29:02 +0100

    asdasd

--- src/Rysunek.java
+++ src/Rysunek.java
@@ -13,10 +13,12 @@ public class Rysunek extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		g.setColor(Color.BLACK);
-		g.drawLine(0, 0, 400, 300);
 		g.setColor(Color.BLUE);
 		g.drawLine(100,100,200,200);
+		g.drawLine(200,200,300,300);
+		g.setColor(Color.BLACK);
+		g.drawLine(0, 0, 400, 300);
+		
 	}
 
 }
