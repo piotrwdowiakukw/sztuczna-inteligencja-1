fd710fb0f018d3e7b1ded1be6a91a9a8fb36b8c2
Piotr Wdowiak <piotrwdowiak@op.pl>
2014-01-03 10:54:21 +0100

    first initial commit

---
+++ src/Panel.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+
+public class Panel {
+
+	public static void main(String[] args) {
+		new Panel();
+	}
+	
+	public Panel() {
+		JFrame jFrame = new JFrame();
+		jFrame.setTitle("Sztuczna inteligencja 1");
+		jFrame.setSize(400,300);
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+}
---
+++ src/Rysunek.java
@@ -0,0 +1,17 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+
+public class Rysunek extends JPanel {
+	
+	public Rysunek() {
+		setBackground(Color.BLUE);		
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	}
+
+}
