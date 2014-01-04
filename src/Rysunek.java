7f4bf72a78fb6c095ff34b7636978f572193cf5c
Piotr Wdowiak <piotrwdowiak@op.pl>
2014-01-04 15:17:32 +0100

    this messege is much more nicer and pretty cute

--- src/Rysunek.java
+++ src/Rysunek.java
@@ -12,8 +12,7 @@ public class Rysunek extends JPanel {
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		g.setColor(Color.BLUE);
+		//Mój komentarz który chcia³bym dodaæ mimo wszystko
 		g.drawLine(100,100,200,200);
 		g.drawLine(200,200,300,300);
 		g.setColor(Color.BLACK);
